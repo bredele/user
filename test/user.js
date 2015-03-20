daa241b297e89024d763ed1debf8c24b88241499
bredele <olivier.wietrich@gmail.com>
2015-03-19 21:12:57 -0600

    set test script using browserify and testling

--- test/user.js
+++ test/user.js
@@ -6,9 +6,13 @@
 var test = require('tape');
 var user = require('..')();
 
-test('API', function(assert) {
+test('api', function(assert) {
 	assert.plan(3);
   assert.equal(typeof user.click, 'function');
   assert.equal(typeof user.press, 'function');
   assert.equal(typeof user.type, 'function');
+});
+
+test('click selector', function(assert) {
+	
 });
